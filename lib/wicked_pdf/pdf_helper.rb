7c44e9a6840ab44f3efe4d341131dc1132c557db
Ivan Lucin <lucin.ivan@gmail.com>
2015-10-17 19:35:33 +0200

    Comment out aliasing functionality

--- lib/wicked_pdf/pdf_helper.rb
+++ lib/wicked_pdf/pdf_helper.rb
@@ -2,17 +2,18 @@ module PdfHelper
   require 'wicked_pdf'
   require 'tempfile'
 
-  def self.included(base)
-    # Protect from trying to augment modules that appear
-    # as the result of adding other gems.
-    return if base != ActionController::Base
-
-    base.class_eval do
-      alias_method_chain :render, :wicked_pdf
-      alias_method_chain :render_to_string, :wicked_pdf
-      after_filter :clean_temp_files
-    end
-  end
+  # Have to remove alias methods so Turbolinks & Wicked pdf gems can play well together
+  # def self.included(base)
+  #   # Protect from trying to augment modules that appear
+  #   # as the result of adding other gems.
+  #   return if base != ActionController::Base
+  #
+  #   base.class_eval do
+  #     alias_method_chain :render, :wicked_pdf
+  #     alias_method_chain :render_to_string, :wicked_pdf
+  #     after_filter :clean_temp_files
+  #   end
+  # end
 
   def render_with_wicked_pdf(options = nil, *args, &block)
     if options.is_a?(Hash) && options.key?(:pdf)
